7eab4e191526f0332ea03a08c194b64d2e41215f
Antonella Morittu <morittuantonella@gmail.com>
2018-01-21 21:04:26 +0100

    further specs on game scenarios

--- lib/rps.rb
+++ lib/rps.rb
@@ -2,12 +2,43 @@ class RPS
   attr_accessor :players
   attr_reader :choices
 
-  POSSIBLE_CHOICES = ['rock', 'paper', 'scissors']
+  POSSIBLE_OPTIONS = ['rock', 'paper', 'scissors']
   DEFAULT_PLAYER_NAMES = ['Player 1', 'Player 2']
 
   def initialize(args = {})
-    @choices = POSSIBLE_CHOICES
+    @choices = POSSIBLE_OPTIONS
     @players = args[:players] || DEFAULT_PLAYER_NAMES
   end
+
+  def player_1
+    @players[0]
+  end
+
+  def player_2
+    @players[1]
+  end
+
+def choice(player)
+  { player: player, choice: POSSIBLE_OPTIONS.sample }
+end
+
+def outcome(player_one, player_two)
+  if player_one[:choice] == 'rock' && player_two[:choice] == 'scissors'
+    "Winner is #{player_one[:player]}!"
+  elsif player_one[:choice] == 'scissors' && player_two[:choice] == 'rock'
+    "Winner is #{player_two[:player]}!"
+  elsif player_one[:choice] == 'scissors' && player_two[:choice] == 'paper'
+    "Winner is #{player_one[:player]}!"
+  elsif player_one[:choice] == 'paper' && player_two[:choice] == 'scissors'
+    "Winner is #{player_two[:player]}!"
+  elsif player_one[:choice] == 'paper' && player_two[:choice] == 'rock'
+    "Winner is #{player_one[:player]}!"
+  elsif player_one[:choice] == 'rock' && player_two[:choice] == 'paper'
+    "Winner is #{player_two[:player]}!"
+  elsif player_one[:choice] == player_two[:choice]
+    "It's a tie!"
+  end
+end
+
 
 end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -35,4 +35,90 @@ it 'accepts custom player names on initialize' do
 new_game = RPS.new(players: ['Antonella', 'Thomas'])
   expect(new_game.players.first).to eq 'Antonella'
 end
+
+it {is_expected.to respond_to :player_1}
+it {is_expected.to respond_to :player_2}
+
+it '#player_1 returns "Player 1"' do
+  expect(subject.player_1).to eq 'Player 1'
+end
+
+it '#player_2 returns "Player 2"' do
+  expect(subject.player_2).to eq 'Player 2'
+end
+
+it {is_expected.to respond_to :choice}
+
+it '#choice returns a valid option' do
+  player = 'Antonella'
+  choice = subject.choice(player)
+  expect(RPS::POSSIBLE_OPTIONS).to include choice[:choice]
+end
+
+it '#choice returns object with info about player and option' do
+  player = subject.player_1
+  choice = subject.choice(player)
+  expect(choice[:player]).to eq 'Player 1'
+end
+
+describe 'the #outcome' do
+  it {is_expected.to respond_to :outcome}
+
+  it 'rock beats scissors - first scenario' do
+      choice_1 = {player: 'Player 1', choice: 'rock'}
+      choice_2 = {player: 'Player 2', choice: 'scissors'}
+      expect(subject.outcome(choice_1, choice_2)).to eq 'Winner is Player 1!'
+    end
+
+    it 'rock beats scissors - second scenario' do
+      choice_1 = {player: 'Player 1', choice: 'scissors'}
+      choice_2 = {player: 'Player 2', choice: 'rock'}
+      expect(subject.outcome(choice_1, choice_2)).to eq 'Winner is Player 2!'
+    end
+
+    it 'scissors beats paper - first scenario' do
+      choice_1 = {player: 'Player 1', choice: 'scissors'}
+      choice_2 = {player: 'Player 2', choice: 'paper'}
+      expect(subject.outcome(choice_1, choice_2)).to eq 'Winner is Player 1!'
+    end
+
+    it 'scissors beats paper - second scenario' do
+      choice_1 = {player: 'Player 1', choice: 'paper'}
+      choice_2 = {player: 'Player 2', choice: 'scissors'}
+      expect(subject.outcome(choice_1, choice_2)).to eq 'Winner is Player 2!'
+    end
+
+    it 'paper beats rock - first scenario' do
+      choice_1 = {player: 'Player 1', choice: 'paper'}
+      choice_2 = {player: 'Player 2', choice: 'rock'}
+      expect(subject.outcome(choice_1, choice_2)).to eq 'Winner is Player 1!'
+    end
+
+    it 'paper beats rock - second scenario' do
+      choice_1 = {player: 'Player 1', choice: 'rock'}
+      choice_2 = {player: 'Player 2', choice: 'paper'}
+      expect(subject.outcome(choice_1, choice_2)).to eq 'Winner is Player 2!'
+    end
+
+    it 'rock ties with rock ' do
+      choice_1 = {player: 'Player 1', choice: 'rock'}
+      choice_2 = {player: 'Player 2', choice: 'rock'}
+      expect(subject.outcome(choice_1, choice_2)).to eq 'It\'s a tie!'
+    end
+
+    it 'scissors ties with scissors ' do
+      choice_1 = {player: 'Player 1', choice: 'scissors'}
+      choice_2 = {player: 'Player 2', choice: 'scissors'}
+      expect(subject.outcome(choice_1, choice_2)).to eq 'It\'s a tie!'
+    end
+
+    it 'paper ties with paper ' do
+      choice_1 = {player: 'Player 1', choice: 'paper'}
+      choice_2 = {player: 'Player 2', choice: 'paper'}
+      expect(subject.outcome(choice_1, choice_2)).to eq 'It\'s a tie!'
+    end
+
+  end
+
+
 end
